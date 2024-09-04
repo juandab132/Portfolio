ed141a75c25300b7529c1d9c9c1a3e026098d29d
Juan sanchez <you@example.com>
2024-09-04 17:15:11 -0500

    agregando archivos

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen bg-cyan-300">
+      <h1 className="text-4xl text-cyan-800 font-extrabold">My Portfolio</h1>
+    </main>
+  );
+
+}
