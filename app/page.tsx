a67b57b2425f6b43cfe7b93d131152d94044afb2
Juan sanchez <you@example.com>
2024-10-22 19:53:42 -0500

    cambiando titulo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen bg-cyan-300">
-      <h1 className="text-4xl text-cyan-800 font-extrabold">My Portfolio</h1>
+      <h1 className="text-4xl text-cyan-800 font-extrabold">My Portafolio</h1>
     </main>
   );
 
